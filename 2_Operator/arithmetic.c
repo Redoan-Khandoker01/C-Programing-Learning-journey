88edb365be0609028920cab0fdff15d4b7aeb1e2
Redoan Khandoer <redoankhandoker8066@gmail.com>
2025-10-09 14:39:59 +0600

    make folder

---
+++ 2_Operator/arithmetic.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+int main()
+{
+
+   //Arithmetic Operator ( + - * / % ) using for calculation
+   // (% -mod) mod using to get  ভাগশেষ 
+
+   int a = 25; 
+   int b =  5 ;  
+  // intiger value 
+
+   int sum =a+b; 
+   int sub = a-b;
+   int div = a/b;
+   int Multi= a*b;
+   int mod = a%b;
+
+    printf("1 sumation = %d \n", sum);
+    printf("2 subtraction = %d\n",sub);
+    printf("3 division %d\n",div);
+    printf("4 Multiplication %d\n", Multi);
+    printf("5 Mod = %d\n", mod);
+
+    // thats all about Arithmetic Operator.
+
+    // Relational Operator( >,<,>=, <= ,=, !=)
+        // we used this Operator to compere 2 value 
+
+    
+
+
+
+
+
+
+
+
+
+    return 0;
+}
