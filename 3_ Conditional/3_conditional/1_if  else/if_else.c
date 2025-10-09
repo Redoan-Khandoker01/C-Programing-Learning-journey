80a64b2b4cc4d7a20f0937a7fb114525dd524504
Redoan Khandoer <redoankhandoker8066@gmail.com>
2025-10-09 14:02:13 +0600

    c

---
+++ 3_ Conditional/3_conditional/1_if  else/if_else.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int main()
+{
+
+    // IF_ELSE it is working condition base.
+
+    // if (i have 100 tka :)
+    //         {
+    //             i will eat Burger.
+    //         }
+    // else:
+    //         {
+    //             i won't  eat Burger.
+
+    // No we can wright Code in C //
+
+     // Task -1 
+    int taka;
+    scanf("%d", &taka);
+    if (taka >= 100) // if =যদি শর্ত
+    {
+        printf("i will eat Burger");
+    }
+    else // নাহলে শর্ত ।
+    {
+        printf("i will not eat Burger");
+    }
+
+    
+
+    return 0;
+}
---
+++ 3_ Conditional/3_conditional/2_else_if/else_if.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main()
+{
+
+   /* 
+    if (i have 100 tk)
+            {
+              i will go to kuyakata
+            } 
+    else if(i have 50 tk )
+            {
+                i will go to barishal
+            }          
+   else 
+            {
+               i will not go anywhere.
+            } 
+   */
+
+   // ok now we can wright code 
+
+    int taka;
+    taka = 40;
+    if (taka>=100)
+        {
+            printf("i will go to kuakata");
+        }
+    else if (taka >=40) {
+        printf("i will go barishal");
+    }
+    else 
+        printf("i will sleep on my bed");
+    return 0;
+}
