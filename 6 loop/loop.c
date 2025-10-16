b30a228a99d0e8ff59a82369f842df76741bf04c
Redoan Khandoer <redoankhandoker8066@gmail.com>
2025-10-16 21:51:41 +0600

    loop task

---
+++ 6 loop/loop.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+
+   int n;
+   int x;
+   scanf("%d",&n);
+   for(int i=1;i<=n;i++)
+    {
+        scanf("%d",&x);
+        printf("%d ",x);
+        
+    }
+    return 0;
+}
