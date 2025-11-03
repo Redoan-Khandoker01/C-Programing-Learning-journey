d9daec31f6e0bfc3d52d131a92f3902bbf085998
Redoan Khandoker <redoankhandoker8066@gmail.com>
2025-11-03 20:08:17 +0600

    sumation

---
+++ sum of tow value/sumOf_tow_value.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int array[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d",&array[i]);
+    }
+    int jogfol;
+    scanf("%d",&jogfol);
+
+for (int i = 0; i < n-1; i++) 
+
+{
+    for(int j=i+1;j<n;j++)
+    {
+        if(array[i]+array[j]==jogfol)
+        {
+            printf("%d %d\n", array[i],array[j]);
+        }
+
+        
+    }
+}
+
+
+    //  for (int i = 0; i < n; i++)
+    // {
+    //     printf("%d ",array[i]);
+    // }
+    // printf("%d",jogfol);
+
+return 0;
+}
