4489162ed750e47e15bcc5fdd7e1018d8953c344
Redoan Khandoker <redoankhandoker8066@gmail.com>
2025-11-03 16:06:35 +0600

    nested loop

---
+++ Nestedloop/starPrint.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+
+int n;
+scanf("%d",&n);
+int star =1;
+for (int i = 0; i < n; i++) // for printing line from n ;
+{
+    for (int i = 0; i <star; i++) // for printing star from 1 to n line 
+    {
+        printf("*");
+    }
+    
+
+
+    printf("\n");
+    star+=2;
+}
+
+
+return 0;
+}
