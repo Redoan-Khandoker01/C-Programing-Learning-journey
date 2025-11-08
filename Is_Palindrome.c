a4b0c489ec1424517738f7e85854d27985c05268
Redoan Khandoker <redoankhandoker8066@gmail.com>
2025-11-09 00:06:41 +0600

    word

--- Is_Palindrome.c
+++ Is_Palindrome.c
@@ -1,11 +1,30 @@
 #include<stdio.h>
 
-int is_palindrome()
+int is_palindrome(char s [1001])
 {
+        int len=strlen(s)-1;
+        
+        int i=0;
+        int j = strlen(s);
+        for(int i=0;i<len;i++)
+        {
+            for(int j=1;j<=s;j++)
+            {
+                if(s[i]==s[j])
+                {
+                
+                }
+                
+                  i++;
+                  j--
+                
+            }
+ 
+
+        }
 
 
-
-    return;
+    return 1;
 }
 
 int main()
@@ -13,7 +32,17 @@ int main()
 char s[1001];
 scanf("%s",&s);
 
-int take =is_palindrome()
+int take =is_palindrome(s);
+
+
+if (take ==1)
+        {
+            printf("Palindrome\n");
+        }
+    else
+        {
+            printf("Not Palindrome\n");
+        }
 
 
 
