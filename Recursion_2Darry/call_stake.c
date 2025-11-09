a8e44f062829a63e4884e5aca568584b26b50b97
Redoan Khandoker <redoankhandoker8066@gmail.com>
2025-11-09 10:17:46 +0600

    call stak

---
+++ Recursion_2Darry/call_stake.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+
+void mello()
+{
+    printf("mello\n");
+}
+void gello()
+{
+    printf("gello\n");
+    mello();
+}
+void hello()
+{
+    printf("hello\n");
+    gello();
+}
+
+
+
+int main()
+{
+    printf("Hi\n");
+    hello();
+
+
+return 0;
+}
